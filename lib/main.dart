b9dd9b4711230549196108481fd02de623ac0256
GabrielLeao <90697922+GabrielLeao02@users.noreply.github.com>
2022-05-08 00:41:23 -0300

    ajuste layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
+          backgroundColor: Colors.lightGreen,
           title: Center(
-            child: Text('I am developer, Gabriel Leao!'),
+            child: Text('I am developer mobile, Gabriel Leao!'),
           ),
         ),
         backgroundColor: Colors.blueGrey[200],
-        body: Center(
-          child: Image(
-            image: AssetImage(
-                'images/Gabrielleao.jpg'),
+        body: SafeArea(
+          child: Container(
+            child: Center(
+              child: Image(
+                height: 300.0,
+                width: 250.0,
+                image: AssetImage("images/Gabrielleao.jpg"),
+              ),
+            ),
           ),
         ),
-    ),
-  ));
+      ),
+    );
+  }
 }
